f07a2a879143c5550ad9279054bc1d9e5aae6d3d
Tom Hören <horen.tom@gmail.com>
2021-02-12 11:11:00 +0100

    Update __init__.py

--- starz/__init__.py
+++ starz/__init__.py
@@ -2,5 +2,5 @@
 # Distributed under the terms of the MIT License
 import os
 
-__version__ = "0.1.0.dev0"
+__version__ = "0.1.1.dev0"
 __tool_name__ = os.path.split(os.path.abspath(os.path.dirname(__file__)))[-1]
